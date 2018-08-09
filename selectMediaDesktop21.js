60c300dc463ce8958ed724e35fe546ab141b5fc9
shanicomvert <shani.g@comvert.io>
2018-08-09 17:11:00 +0300

    all responses ready accept 2

--- selectMediaDesktop21.js
+++ selectMediaDesktop21.js
@@ -1,5 +1,5 @@
 var formSubmitBtn = document.getElementById("comp-jitywe7w");
-var html = '<div style="position: absolute; top: 586px; height: 42px; min-height: 32px; width: 400px; font-size:18px; left:270px">'+
+var html = '<div style="position: absolute; top: 576px; height: 42px; min-height: 32px; width: 400px; font-size:18px; left:270px">'+
 '        <span>Do you need help?</br>Please feel free to'+
 '            <a href="https://www.selectmedia.asia/contact-selectmedia" target="_blank" style="color: #A80303; border-bottom: #A80303">contact us</a>.'+
 '        </span>'+
