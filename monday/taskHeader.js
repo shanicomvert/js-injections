a036b699aaa0a3df5ed2f5a378b44cf00aa379fe
shanicomvert <shani.g@comvert.io>
2018-08-14 13:03:42 +0300

    change selectmedia files name and add monday's scripts

---
+++ monday/taskHeader.js
@@ -0,0 +1,4 @@
+var header = document.getElementsByClassName('mobile-hidden fw300 video')[0];
+
+header.innerHTML = 'Task&nbsp;management';
+header.innerText = 'Task management';
---
+++ monday/teamHeader.js
@@ -0,0 +1,4 @@
+var header = document.getElementsByClassName('mobile-hidden fw300 video')[0];
+
+header.innerHTML = 'Team&nbsp;management';
+header.innerText = 'Team management';
