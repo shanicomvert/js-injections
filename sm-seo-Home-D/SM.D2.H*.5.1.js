ab19911ce4d1b6f09b5701e7715879412af9f941
shanigreidy <shanig190@gmail.com>
2018-09-02 02:26:28 +0300

    continue sm-d

--- sm-seo-Home-D/SM.D2.H*.5.1.js
+++
@@ -1,7 +0,0 @@
-var img = document.getElementById('comp-jiboxkpximgimage');
-img.src = 'https://cdn.comvert.io/vendor/Selectmedia/Images/hands%20(crew).png'
-var html = '<div style="margin-left: calc((100% - -1194px) * 0.5);top: 145px;position: absolute;"><img src="https://cdn.comvert.io/vendor/Selectmedia/Images/globs.png"></img></div>';
-var div = document.getElementById('comp-jiboxka2');
-div.insertAdjacentHTML('beforebegin', html);
-html = '<div style="display:flex; flex-flow:row;margin-left: calc((100% - 822px) * 0.5);"><a style="z-index:10" href="mailto:info@selectmedia.asia"> <img src="https://cdn.comvert.io/vendor/Selectmedia/Images/email.png"></img></a><img style="margin-left:25px" src="https://cdn.comvert.io/vendor/Selectmedia/Images/phone.png"></img></div>';
-div.insertAdjacentHTML('beforebegin', html);
--- sm-seo-Home-D/sm.js
+++ sm-seo-Home-D/sm.js
@@ -1,29 +1,38 @@
-var video4 = document.getElementById('video4');
-var video4cover = document.getElementById('video4cover');
-var video4content = document.getElementById('video4content');
-video4cover.style.display = 'none';
-var video3 = document.getElementById('video3');
-var video3cover = document.getElementById('video3cover11');
+var V1 = document.getElementById('V1');
+var video1 = document.getElementsByClassName('video1')[0];
+var V3 = document.getElementById('V3');
+var video3 = document.getElementsByClassName('video3')[0];
+// var video4cover = document.getElementById('video4cover');
+// var video4content = document.getElementById('video4content');
+// video4cover.style.display = 'none';
+// var video3 = document.getElementById('video3');
+// var video3cover = document.getElementById('video3cover11');
 // var video4content = document.getElementById('video4content');
 // video3cover.style.display = 'none';
 window.sud = this;
 
-function onMouseEnterV4(){
+function onMouseEnterV1(){
     var that = window.sud;
-    that.video4.style.display = 'none';
-    that.video4content.style.display = 'none';
-    that.video4cover.style.display = 'block';
+    that.V1.style.background = 'rgba(139, 0, 0, 1)';
+    that.video1.style.opacity = '1';
+    // that.video4content.style.display = 'none';
+    // that.video4cover.style.display = 'block';
 }
 
-function onMouseLeaveV4(){
+function onMouseLeaveV1(){
     var that = window.sud;
-    that.video4.style.display = 'block';
-    that.video4content.style.display = 'block';
-    that.video4cover.style.display = 'none';
+    var that = window.sud;
+    that.V1.style.background = 'rgba(139, 0, 0, 0.68)';
 }
-
 function onMouseEnterV3(){
     var that = window.sud;
-    that.video3.style.display = 'none';
-    that.video3cover.style.display = 'block';
+    that.V3.style.background = 'rgba(139, 0, 0, 1)';
+    // that.video4content.style.display = 'none';
+    // that.video4cover.style.display = 'block';
+}
+
+function onMouseLeaveV3(){
+    var that = window.sud;
+    var that = window.sud;
+    that.V3.style.background = 'rgba(139, 0, 0, 0.68)';
 }
