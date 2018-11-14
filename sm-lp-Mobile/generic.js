40ecedf28681b3aeaab9a0cabfc28a875b005473
Shani <shanicomvert>
2018-11-14 18:40:05 +0200

    add select outstream mobile responses

---
+++ sm-lp-Mobile/generic.js
@@ -0,0 +1,40 @@
+var style = '<style>'+
+'#outstreamJoinBtnCtn, #whyChooseJoinBtnCtn, #marketplaceJoinBtnCtn{'+
+'position: relative;'+
+'margin: 23px 0px 0px 0;'+
+'left: 77px;'+
+'grid-area: 3 / 1 / 4 / 2;'+
+'justify-self: start;'+
+'align-self: start;'+
+'margin-left: calc((100% - 264px) * 0.5);'+
+'}'+
+'</style>';
+
+document.body.insertAdjacentHTML('beforeend', style);
+
+var numberOne = document.getElementById("comp-jl6b1ynh");
+numberOne.innerHTML = '<div style="display:flex;width:100%;"><h1 class="font_0" style="font-size:23px;width:94%;">OutStream Video SSP</h1><a style="width:20%;position: absolute;right: -21px;width: 20%;top: -15px;" href="https://imgbb.com/"><img style="width:80px;" src="https://cdn.comvert.io/vendor/Selectmedia/Images/no1asia.png" alt="no1asia" border="0"/></a></div>';
+document.getElementsByClassName('font_7')[6].innerHTML = '<span style="font-size:18px"><a href="#comp-jiboxk94inlineContent" style="color:black;border-bottom:2px solid black;font-weight:bold;">Select Media</a> helps publishers to monetize their video and mobile inventory through its full - stack outstream video ssp technology and access to its unique marketplace</span>';
+document.getElementById('comp-jlcda103inlineContent').style.height = '540px';
+document.getElementById('comp-jj51j8hwinlineContent-gridWrapper').style.marginLeft = 'calc((100% - 138px) * 0.5)';
+document.getElementById('comp-jj47odlr').style.width = '194px';
+document.getElementById('comp-jj47odlr').style.marginLeft = 'calc((100% - 490px) * 0.5)';
+document.getElementById('comp-jj47odlr').style.marginTop = '145px';
+document.getElementById('comp-jj0kp8h4').style = 'margin-bottom: 0px;margin-top: 10px;';
+document.getElementById('comp-jj47iafr').style.marginTop = '0';
+document.getElementById('comp-jiboxgf4').insertAdjacentHTML('afterend', '<div id="outstreamJoinBtnCtn" data-align="center" data-disabled="false" data-margin="0" data-should-use-flex="true" data-width="119" data-height="30" style="height: 30px; min-height: 19px; width: 119px;" class="style-jl6b1ysp" data-state="mobile shouldUseFlex center"><a href="https://www.selectmedia.asia/join-as-a-publisher-selectmedia" target="_self" role="button" id="outstreamJoinBtn" class="g-transparent-a style-jl6b1ysplink"><span style="font-size:14px" id="outstreamJoinBtnLabel" class="style-jl6b1ysplabel">J O I N   </span></a></div>')
+document.getElementById('comp-jiboxkek').insertAdjacentHTML('afterend', '<div id="whyChooseJoinBtnCtn" data-align="center" data-disabled="false" data-margin="0" data-should-use-flex="true" data-width="119" data-height="30" style="height: 30px; min-height: 19px; width: 119px;" class="style-jl6b1ysp" data-state="mobile shouldUseFlex center"><a href="https://www.selectmedia.asia/join-as-a-publisher-selectmedia" target="_self" role="button" id="whyChooseJoinBtn" class="g-transparent-a style-jl6b1ysplink"><span style="font-size:14px" id="whyChooseJoinBtnLabel" class="style-jl6b1ysplabel">J O I N   </span></a></div>')
+document.getElementById('comp-jiboxgg3inlineContent-gridWrapper').style.display = 'none';
+document.getElementById('comp-jiboxgqe').style.display = 'none';
+document.getElementById('comp-jiboxgj5inlineContent-gridWrapper').style.display = 'none';
+document.getElementById('comp-jiboxgskinlineContent-gridWrapper').style.display = 'none';
+document.getElementById('comp-jiboxgmi').style.display = 'none';
+document.getElementById('comp-jiboxgw4').style.display = 'none';
+document.getElementById('comp-jiboxgrf').style.display = 'none';
+document.getElementById('comp-jiboxgpc').style.display = 'none';
+document.getElementById('comp-jiboxgxc').style.display = 'none';
+document.getElementById('comp-jj0ggejxlabel').style = "font-size: 13.6936px;margin-right: 30px;";
+document.getElementById('comp-jj0ggendlabel').style = "font-size: 13.6936px;margin-left: 23px;";
+
+
+//carousel in react
--- sm-lp-Mobile/sm-lp-mobile5.js
+++
@@ -1,3 +0,0 @@
-var jumboCtn = document.getElementById("comp-jgy74w2sinlineContent");
-var html = '<a href="https://imgbb.com/"><img src="https://image.ibb.co/jjutVe/no1asia.png" alt="no1asia" border="0" style="    position: absolute;width: 100px;top: -41px;z-index: 1000;right: -15px;"/></a>';
-jumboCtn.insertAdjacentHTML('beforebegin', html);
